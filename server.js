59d9cff1972ae1b1c31d2084cde42d5cac6e4d99
Nandkishor Sonune <nandkishorsonune0@gmail.com>
2023-02-02 12:07:30 +0530

    all functionality uploaded

---
+++ server.js
@@ -0,0 +1,154 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://nandkishor:nandkishor@cluster0.dndtbxs.mongodb.net/formDB', {useNewUrlParser: true, useUnifiedTopology: true })
+
+const app = express()
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors({
+    methods: ['POST'],
+    credentials: true
+}))
+
+// Collection Schema
+const formSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'Please Provide First Name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please Provide Email']
+    },
+    phoneNum: {
+        type: String,
+        required: [true, 'Please Provide Phone Number']
+    },
+    dob: {
+        type: String,
+        required: [true, 'Please Select your Birthdate']
+    },
+})
+
+
+// Collection Model
+const Form = mongoose.model('form', formSchema)
+
+
+app.get('/', function(req, res){
+    res.status(200).send("Stack Fusion Server!")
+})
+
+app.post('/save-data', function(req, res){
+    
+    const firstName = req.body.firstName
+    const email = req.body.email
+    const phoneNum = req.body.phoneNum
+    const dob = req.body.dob
+
+    //Save to data in DB
+    const formData = Form({
+        firstName : firstName,
+        email : email,
+        phoneNum : phoneNum,
+        dob : dob,
+    })
+
+    formData.save(function(error){
+        if(error){
+            console.log(error)
+            //In case of error, send to client
+            res.status(500)
+        }else{
+            console.log('Form data saved')
+        }
+    })
+
+    
+    //Send confirmation email after data is saved to DB
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'nandkishorsonune0@gmail.com',
+            pass: "rycpjljlcptetsnx"
+        }
+    });
+
+    const mailOptions = {
+        from: 'nandkishorsonune0@gmail.com',
+        to: email,
+        subject: 'StackFusion User-Form Confirmation',
+        text: `Form data submited :
+
+            First Name: ${firstName},
+            Email: ${email},
+            Phone Number: ${phoneNum},
+            DoB: ${dob}
+        `
+    };
+
+    transporter.sendMail(mailOptions, function(err, info){
+        
+        if(err){
+            console.log(err);
+        }else{
+            console.log(info);
+            console.log("Successfully submitted form.");
+        }
+    });
+
+    res.status(200).json({message : "Form data saved and confirmation email sent."})
+ 
+// transporter.sendMail({
+//   from: 'nandkishorsonune0@gmail.com',
+//   to: email,
+//   subject: 'StackFusion User Form Confirmation',
+//   text: `A new form has been submitted:
+//             First Name : ${firstName},
+//             Email: ${email},
+//             Phone Number : ${phoneNum},
+//             DoB : ${dob}`
+  
+
+// }, (err, info) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(`Email sent: ${info.response}`);
+//   }
+//   res.status(200).json({message : "Form data saved and confirmation email sent."})
+// });
+})
+
+
+
+
+
+// Read all forms
+app.get('/read-forms', function(req, res){
+
+    Form.find(function(error, foundForms){
+        if(error){
+            console.log(error)
+            res.status(500)
+        }else{
+            console.log(foundForms)
+            res.status(200).send(foundForms)
+        }
+    })
+})
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+
+app.listen(port, function(){
+    console.log(`Server running on PORT : ${port}`)
+})
